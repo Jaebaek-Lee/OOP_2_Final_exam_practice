a34464e6d0bc1d50473289db8c884ab482fb6713
Lee Jae-Baek <103738589+ThinkingVincent@users.noreply.github.com>
2022-11-27 19:50:49 +0900

    다형성 연습문제 2

--- polymorphism_practice.cpp
+++ polymorphism_practice.cpp
@@ -2,45 +2,52 @@
 
 using namespace std;
 
-class Circle {
+class  Person {
 public:
-	Circle(double radius) {
-		this->radius = new double(radius);
-	}
-	double area() {
-		return *radius * *radius * 3.14;
-	}
-	~Circle() {
-		cout << "Circle's radius: " << *radius << "cm\n";
-		delete radius;
+	Person(string name) : name{name} {}
+	virtual void display() {
+		cout << "Name: " << name << '\n';
 	}
 private:
-	double* radius;
+	string name;
 };
 
-class Cone : public Circle {
+class Student : public Person {
 public:
-	Cone(double radius, double high) : Circle(radius), high { new double(high) } {}
-	~Cone() {
-		cout << "Cone's volume: " << area() * *high / 3 << " cm^3\n";
-		delete high;
+	Student(string name, int num, string major) 
+		: Person(name), studentNumber{ num }, major{ major } {}
+	void display() override {
+		Person::display();
+		cout << "Student ID: " << studentNumber << '\n';
+		cout << "Department: " << major << '\n';
 	}
 private:
-	double* high;
+	int studentNumber;
+	string major;
 };
 
-class Cylinder : public Circle {
+class Professor : public Person {
 public:
-	Cylinder(double radius, double high) : Circle(radius), high{ new double(high) } {}
-	~Cylinder() {
-		cout << "Cylinder's volume: " << area() * *high << " cm^3\n";
-		delete high;
+	Professor(string name, string major, string field)
+		: Person(name), major{major}, field{field} {}
+	void display() override {
+		Person::display();
+		cout << "Department: " << major << '\n';
+		cout << "Field: " << field << '\n';
 	}
 private:
-	double* high;
+	string major;
+	string field;
 };
 
 int main() {
-	Cone cone(4, 5);
-	Cylinder cylinder(5, 4);
+	Person* person[3] = {
+		new Student("홍길동", 12345678, "컴퓨터공학과"),
+		new Student("김철수", 87654321, "컴퓨터공학과"),
+		new Professor("김인하", "컴퓨터공학과", "머신러닝")
+	};
+	for (int i = 0; i < 3; ++i) {
+		person[i]->display();
+		cout << endl;
+	}
 }
